647a200084f9d53ef6970efa6abc084969a01e4c
Dave Collins <dave@hopest.net>
2013-10-10 17:58:14 +1100

    Made a few decisions and implement simple cases

--- node.py
+++ node.py
@@ -4,17 +4,23 @@ class Node(object):
     def __init__(self, element):
         self._element = element
 
+    def _make_new_node_or_leaf(self, new_element):
+        if(len(list(new_element.iter())) == 1):
+            return new_element.text
+        else:
+            return Node(new_element)
+    
+
     def __getattr__(self, attr):
         if attr in self.__dict__:
+            print "we has " , attr
             return self.__dict__[attr]
         l = list(self._element.iter(attr))
         print "len(l) = %d" % len(l) 
-        if len(l) > 0:
-            new_element = l[0]
-            print type(new_element)
-            if(len(list(new_element.iter())) == 1):
-                return new_element.text
-            return Node(new_element)
+        if len(l) == 1:
+            return self._make_new_node_or_leaf(l[0])
+        elif len(l) > 1:
+            return tuple([self._make_new_node_or_leaf(e) for e in l])
                 
         
         raise AttributeError("%r object has no attribute %r" %
--- test_doc.py
+++ test_doc.py
@@ -4,13 +4,15 @@ import unittest
 class DocTestCases(unittest.TestCase):
     def testsimple(self):
         d = doc.Doc('gui.xml')
-        self.assertEquals("a","a")
-        print d.guidata.skin.scoreFontName
         self.assertEquals(d.guidata.skin.scoreFontName, "Tahoma Bold")
-        """
-        <guidata>
-            <skin>
-                    <scoreFontName>"""
 
 
+    def testlist(self):
+        d = doc.Doc('gui.xml')
+        self.assertEquals(type(d.guidata.skin.nextBlockX.line),  tuple)
+        self.assertEquals(d.guidata.skin.nextBlockX.line[1], "206" )
 
+    def testlistofnodes(self):
+        d = doc.Doc('simple.xml')
+        self.assertEquals(type(d.breakfast_menu.food),  tuple)
+        self.assertEquals(d.breakfast_menu.food[1].name, "Strawberry Belgian Waffles" )
