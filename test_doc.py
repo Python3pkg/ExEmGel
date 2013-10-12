4847d206fd7f7567939e191c4d65fe1954064655
Dave Collins <dave@hopest.net>
2013-10-12 21:53:12 +1100

    Added initial version of read_config

--- test_doc.py
+++ test_doc.py
@@ -1,27 +1,10 @@
 import doc
 import unittest
-import StringIO
+import data_for_testing
 
 class DocTestCases(unittest.TestCase):
     def testsimple(self):
-        s = StringIO.StringIO("""\
-<?xml version="1.0"?>
-<guidata>
-    <skin>
-        <scoreFontName>Tahoma Bold</scoreFontName>
-        <scoreFontHeight>20</scoreFontHeight>
-        <blockSize>16</blockSize>
-        <nextBlockX>
-            <line>205</line>
-            <line>206</line>
-            <line>207</line>
-            <line>105</line>
-        </nextBlockX>
-        <backgroundFile>"back.bmp"</backgroundFile>
-    </skin>
-</guidata>
-""")
-        d = doc.Doc(s)
+        d = doc.Doc(data_for_testing.simple_xml_file_obj())
         self.assertEquals(d.guidata.skin.scoreFontName, "Tahoma Bold")
 
 
