ee4b7181c5c81ba224705d09f91c050dd38583fd
Dave Collins <dave@hopest.net>
2013-10-13 14:00:28 +1100

    Inital setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
-with open('README.txt') as file:
+with open('README.rst') as file:
     long_description = file.read()
 
 setup(
-    name='TowelStuff',
-    version='0.1.dev1',
-    packages=['towelstuff',],
+    name='ExEmGel',
+    version='0.1',
+    packages=['exemgel',],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=long_description,
     classifiers=[
-        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 2.6',
     ],
 )
