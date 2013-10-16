dcba0ab8a462d870151e395629cef67bd66adebb
Dave Collins <Dave@hopest.net>
2013-10-16 12:53:12 +1100

    Updated readme and licence

--- README.md
+++
@@ -1,4 +0,0 @@
-ExEmGel
-=======
-
-ExEmGel is a wrapper around the python ElementTree library. It allow clean and simple access to xml.
--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     name='ExEmGel',
     version='0.1',
     packages=['exemgel',],
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+    license='MIT',
     long_description=long_description,
     classifiers=[
         'Programming Language :: Python :: 2.6',
